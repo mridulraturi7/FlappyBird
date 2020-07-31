d37fad4bae620a5f71dbf6e56efe0dac75f45f22
mridulraturi7 <mridulraturi7@gmail.com>
2020-07-31 12:05:59 +0530

    [First Update] Background and Ground Images Displayed

--- main.lua
+++ main.lua
@@ -16,3 +16,25 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+local backgorund = love.graphics.newImage('background.png')
+local ground = love.graphics.newImage('ground.png')
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = false
+    })
+end
+
+function love.draw()
+    push:start()
+
+    love.graphics.draw(backgorund, 0, 0)
+
+    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+
+    push:finish()
+end
