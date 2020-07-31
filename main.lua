82785d4844e4cfbf088ed5d99b2cd6f77410ec23
mridulraturi7 <mridulraturi7@gmail.com>
2020-07-31 12:11:52 +0530

    [ADD] Comments

--- main.lua
+++ main.lua
@@ -16,10 +16,12 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+--background and foreground images to render on screen
 local backgorund = love.graphics.newImage('background.png')
 local ground = love.graphics.newImage('ground.png')
 
 function love.load()
+    --nearest neighbour filter to avoid blurring.
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -42,8 +44,10 @@ end
 function love.draw()
     push:start()
 
+    --draw background image at top left(0, 0)
     love.graphics.draw(backgorund, 0, 0)
 
+    --draw ground image(in front of background) at bootom of screen
     love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
 
     push:finish()
