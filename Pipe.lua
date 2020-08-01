f531bc3d4b791df8a41a4e69031d9d31cebcd111
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-01 21:05:09 +0530

    [ADD] Pipe Class Description

---
+++ Pipe.lua
@@ -0,0 +1,10 @@
+--[[
+    Pipe Class.
+    The Pipe class represents the pipes that randomly spawn
+    in our game, which acts as our primary obstacles. The pipes
+    can stick out a random distance from top and bottom of the screen.
+    When the player collides with one of them, its game over. Rather
+    than our bird atually moving through the screen horizontally, the
+    pipes themselves scroll through the game to give the illusion of
+    player movement. 
+]]
