d3887049bed9b1baad17a30df8909db2e0097c0b
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-01 12:27:59 +0530

    [Implemented] Bird.lua

--- Bird.lua
+++ Bird.lua
@@ -9,5 +9,15 @@
 Bird = Class{}
 
 function Bird:init()
-    self.image = love.graphics.newImage('')
+    self.image = love.graphics.newImage('bird.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    --position bird in the middle of the screen
+    self.x = VIRTUAL_WIDTH/2 - (self.width/2)
+    self.y = VIRTUAL_HEIGHT/2 - (self.height/2)
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
 end
