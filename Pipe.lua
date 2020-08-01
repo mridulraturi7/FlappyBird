d94d7241a73d61498882eab10ae2b6d7e32d121e
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-01 21:27:15 +0530

    [Working] Pipe Spawning

--- Pipe.lua
+++ Pipe.lua
@@ -30,5 +30,5 @@ function Pipe:update(dt)
 end
 
 function Pipe:render()
-    love.graphics.render(PIPE_IMAGE, math.floor(self.x + 0.5), math.floor(self.y))
+    love.graphics.draw(PIPE_IMAGE, math.floor(self.x + 0.5), math.floor(self.y))
 end
