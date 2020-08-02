2d46025cdbd7a65950a56de2befc23f95e1b7e1f
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-02 21:28:35 +0530

    [ADD] PipePair Class Description

---
+++ PipePair.lua
@@ -0,0 +1,7 @@
+--[[
+    Pipepair Class.
+
+    It is used to represent a pair of pipes that stck together as they scroll,
+    providing an opening for the player to jump through in order to score a point.
+]]
+
