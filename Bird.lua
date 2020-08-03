ca217ee9a9eb8b33ee3a6d11d489f111dcf3d9ae
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-03 20:57:40 +0530

    [Implemented] Collision Detection

--- Bird.lua
+++ Bird.lua
@@ -28,7 +28,17 @@ end
     global pipe width and height values. 
 ]]
 function Bird:collides(pipe)
-    
+    -- the 2's are left and top offsets
+    --the 4's are right and bottom offsets
+    --both offsets are used to shrink the bounding box to give the player
+    if (self.x + 2) + (self.width - 4) >= pipe.x and (self.x + 2) <= pipe.x + PIPE_WIDTH then
+        if (self.y + 2) + (self.height - 4) >= pipe.y and (self.y +2) <= pipe.y + PIPE_HEIGHT then
+            return true
+        end
+    end
+
+    return false
+
 end
 
 function Bird:update(dt)
