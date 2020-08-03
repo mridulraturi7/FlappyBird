a205ccb47f5667caa82ca4689e2a6c9642348453
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-03 21:25:13 +0530

    [ADD Description] PlayState.lua

---
+++ states/PlayState.lua
@@ -0,0 +1,6 @@
+--[[
+    PlayState Class.
+    It is the bulk of the game, where the player actually controls the
+    bird and avoids pipes. When the player collides with a pipe, we
+    should go to the GameOver state, where we then go back to the main menu.
+]]
