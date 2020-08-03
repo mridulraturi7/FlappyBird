63d597600057118e27e2ed6c2a8f21213de51b9a
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-03 21:20:55 +0530

    [ADD Description] BaseState.lua

---
+++ states/BaseState.lua
@@ -0,0 +1,9 @@
+--[[
+    BaseState Class.
+    It is used as the base class for all states, so we don't have to
+    define empty methods in each of them. StateMachine requires each
+    State have a set of four "interface" methods that it can reliably
+    call, so by inheriting from this base state, our State classes will
+    all have at least empty versions of these methods even if we don't
+    define them ourselves in the actual classes.
+]]
