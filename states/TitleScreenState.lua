f5a939bb6446249c2828df56ff015d966ba38cba
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-03 23:37:16 +0530

    [Modify] TitleScreenState.lua

--- states/TitleScreenState.lua
+++ states/TitleScreenState.lua
@@ -14,5 +14,9 @@ function TitleScreenState:update(dt)
 end
 
 function TitleScreenState:render()
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Fifty Bird', 0, 64, VIRTUAL_WIDTH, 'center')
 
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Press Enter', 0, 100, VIRTUAL_WIDTH, 'center')
 end
