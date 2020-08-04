5821ad9510bf5086502e84fb6f9f34c09b385a0d
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-04 11:36:31 +0530

    [ADD Class Description] ScoreState.lua

---
+++ states/ScoreState.lua
@@ -0,0 +1,8 @@
+--[[
+    ScoreState Class.
+
+    A simple state used to display the player's score before they
+    transition back into the play state. Transitioned to Score State
+    from the Play State when they collide with a Pipe.
+]]
+
