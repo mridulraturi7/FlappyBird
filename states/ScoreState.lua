51a919634f2d16cdcdaf7211bbc0382e40d98c1c
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-04 21:16:01 +0530

    [Modify] Score UI

--- states/ScoreState.lua
+++ states/ScoreState.lua
@@ -32,10 +32,10 @@ end
 function ScoreState:render()
     --simply render the score to the middle of the screen
     love.graphics.setFont(flappyFont)
-    love.graphics.printf('OOPS! You Lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('OOPS! You Lost!', 0, 24, VIRTUAL_WIDTH, 'center')
 
     love.graphics.setFont(mediumFont)
-    love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Score: ' .. tostring(self.score), 0, 60, VIRTUAL_WIDTH, 'center')
 
-    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press Enter to Play Again!', 0, 236, VIRTUAL_WIDTH, 'center')
 end
