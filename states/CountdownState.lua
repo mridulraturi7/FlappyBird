1a8f0f3533da8a8cc718e51fdc320eebdad1d3b9
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-04 12:11:20 +0530

    [Implemented] CountdownState.lua

--- states/CountdownState.lua
+++ states/CountdownState.lua
@@ -5,3 +5,35 @@
     is about to begin. ransitions to the PlayState as soon as the countdown is complete.
 ]]
 
+CountdownState = Class{__includes = BaseState}
+
+--takes 1 second to count down each time
+COUNTDOWN_TIME = 0.75
+
+function CountdownState:init()
+    self.count = 3
+    self.timer = 0
+end
+
+--[[
+    Keeps track of how much time has passed and decreases count if the
+    timer has exceeded our countdown time. If we have gone down to 0,
+    we should transition to our PlayState.
+]]
+function CountdownState:update(dt)
+    self.timer = self.timer + dt
+
+    if self.timer > COUNTDOWN_TIME then
+        self.timer = self.timer % COUNTDOWN_TIME
+        self.count = self.count - 1
+
+        if self.count == 0 then
+            gStateMachine:change('play')
+        end
+    end
+end
+
+function CountdownState:render()
+    love.graphics.setFont(hugeFont)
+    love.graphics.printf(tostring(self.count), 0, 120, VIRTUAL_WIDTH, 'center')
+end
