27d9117692c495e88f5f4c96d9277599d4f86a17
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-04 12:02:09 +0530

    [ADD Class Desciption] CountdownState.lua

---
+++ states/CountdownState.lua
@@ -0,0 +1,7 @@
+--[[
+    CountdownState Class.
+    
+    It counts down visually on the screen (3, 2, 1) so that the player knows the game
+    is about to begin. ransitions to the PlayState as soon as the countdown is complete.
+]]
+
