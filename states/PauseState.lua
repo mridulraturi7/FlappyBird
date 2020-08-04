eea24598dd02eefa766298dd4d10e25dfd74afb3
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-04 21:42:58 +0530

    [Impemented] PauseState.lua

--- states/PauseState.lua
+++ states/PauseState.lua
@@ -4,3 +4,15 @@
     It is used to pause the game while playing.
 ]]
 
+PauseState = Class{__includes = BaseState}
+
+function PauseState:update(dt)
+    if love.keyboard.wasPressed('P') then
+        gStateMachine:change('play')
+    end
+end
+
+function PauseState:render()
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Game Paused', 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+end
