31f5b651570890be62e630eb6f7f78499b7022cd
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-04 12:51:05 +0530

    [Mdify] Bird.lua

--- Bird.lua
+++ Bird.lua
@@ -46,7 +46,7 @@ function Bird:update(dt)
     --apply gravity to velocity
     self.dy = self.dy + GRAVITY * dt
 
-    if love.keyboard.wasPressed('space') then
+    if love.keyboard.wasPressed('space') or love.mouse.wasPressed(1) then
         self.dy = -5
         sounds['jump']:play()
     end
