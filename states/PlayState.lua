d323bc07174add19b5f31d8ecb23a3175dfdd74b
mridulraturi7 <mridulraturi7@gmail.com>
2020-08-04 11:50:19 +0530

    [Modify] PlayState.lua

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -19,6 +19,9 @@ function PlayState:init()
     self.pipePairs = {}
     self.timer = 0
 
+    --variable to keep track of score
+    self.score = 0
+
     --initialize our last recorded Y value for a gap placement to base other gaps off of
     self.lastY = -PIPE_HEIGHT + math.random(80) + 20
 end
@@ -45,6 +48,15 @@ function PlayState:update(dt)
 
     --for every pair of pipes..
     for k, pair in pairs(self.pipePairs) do
+        --score a point if the pipe has gone past the bird to the left all the way
+        --be sure to ignore it if it is already been scored
+        if not pair.scored then
+            if pair.x + PIPE_WIDTH < self.bird.x then
+                self.score = self.score + 1
+                pair.scored = true
+            end
+        end
+
         --update position of pair
         pair:update(dt)
     end
@@ -66,14 +78,18 @@ function PlayState:update(dt)
     for k, pair in pairs(self.pipePairs) do
         for l, pipe in pairs(pair.pipes) do
             if self.bird:collides(pipe) then
-                gStateMachine:change('title')
+                gStateMachine:change('score', {
+                    score = self.score
+                })
             end
         end
     end
 
     --reset if we get to the ground
     if self.bird.y > VIRTUAL_HEIGHT - 15 then
-        gStateMachine:change('title')
+        gStateMachine:change('score', {
+            score = self.score
+        })
     end
 
 end
@@ -82,6 +98,9 @@ function PlayState:render()
     for k, pair in pairs(self.pipePairs) do
         pair:render()
     end
+
+    love.graphics.setFont(flappyFont)
+    love.graphics.print('SCore: ' .. tostring(self.score), 8, 8)
 
     self.bird:render()
 end
